1c8abb2b41ef57f461001542ba2a171069bbf7e9
Aldrin Cerezo <drincerezo@gmail.com>
2024-06-16 15:10:29 +0800

    added templates and contents

--- app.py
+++ app.py
@@ -4,4 +4,13 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    page_title = "Home"
+    return render_template('index.html', page_title=page_title)
+
+@app.route("/about")
+def about():
+    page_title = "About"
+    return render_template('about.html', page_title=page_title)
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
